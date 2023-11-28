34553b1978c5640f926486fab4b839a4c48d62a6
Jaden W <145929939+jwill1109@users.noreply.github.com>
2023-11-28 18:21:27 -0400

    Update fastestGrad.py

--- App/views/fastestGrad.py
+++ App/views/fastestGrad.py
@@ -1,3 +1,11 @@
 from flask import Blueprint, url_for
 
 from App.controllers import fastestGraduation
+
+import endpoints
+from endpoints import message_types
+from endpoints import message
+from endpoints import remote
+
+@endpoints.api(name="fastestGrad", version = "v1")
+class fastestGradApi(remote.Service)
