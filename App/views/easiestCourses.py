0df404ab1730b70943e49000cf2865ee222f7767
Jaden W <145929939+jwill1109@users.noreply.github.com>
2023-11-28 22:29:55 -0400

    began tp add url and endpoint easiestCourses.py

--- App/views/easiestCourses.py
+++ App/views/easiestCourses.py
@@ -1,2 +1,12 @@
-from flask import Blueprint, jsonify, url_for
+from flask import Blueprint, url_for
 from App.models import Program, ProgramCourses
+
+from App.controllers import easyCourses
+
+import endpoints
+from endpoints import message_types
+from endpoints import message
+from endpoints import remote
+
+@endpoints.api(name="easiestCourses", version = "v1")
+class easiestCoursesApi(remote.Service)
