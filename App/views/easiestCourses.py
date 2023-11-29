0e0a8e5d98a03bc54d4f110ff7f419d41c8bfd91
Jaden W <145929939+jwill1109@users.noreply.github.com>
2023-11-29 14:04:55 -0400

    Update easiestCourses.py

--- App/views/easiestCourses.py
+++ App/views/easiestCourses.py
@@ -10,3 +10,13 @@ from endpoints import remote
 
 @endpoints.api(name="easiestCourses", version = "v1")
 class easiestCoursesApi(remote.Service)
+
+easiestCourses_views = Blueprint('easiestCourses_views', __name__, tamplates_folder='../templates')
+
+@easiestCourses_view.route('/easiestCourses', methods=['GET'])
+def Get_easiest_courses()
+		easyCourses = easyCourses(Student):
+		return easyCourses
+
+
+  
