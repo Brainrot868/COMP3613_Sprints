930408f05f0c6ed61a0a4350bf3e567c089b9649
Jaden W <145929939+jwill1109@users.noreply.github.com>
2023-11-28 22:32:18 -0400

    began adding url and endpoints prioritiseElectives.py

--- App/views/prioritiseElectives.py
+++ App/views/prioritiseElectives.py
@@ -1,2 +1,12 @@
-from flask import Blueprint, url_for, jsonify
+from flask import Blueprint, url_for
 from App.models import Program, ProgramCourses
+
+from App.controllers import prioritzeElectives
+
+import endpoints
+from endpoints import message_types
+from endpoints import message
+from endpoints import remote
+
+@endpoints.api(name="priotiseElectives", version = "v1")
+class priotiseElectivesApi(remote.Service)
