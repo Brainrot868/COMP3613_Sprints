f0fec3e346e202c52add4ca4c3472750cf614b6c
Jaden W <145929939+jwill1109@users.noreply.github.com>
2023-11-29 13:49:23 -0400

    Update fastestGrad.py

--- App/views/fastestGrad.py
+++ App/views/fastestGrad.py
@@ -9,3 +9,11 @@ from endpoints import remote
 
 @endpoints.api(name="fastestGrad", version = "v1")
 class fastestGradApi(remote.Service)
+
+fastestGrad_views = Blueprint('fastestGrad_views', __name__, template_folder='../templates')
+
+#retrieve route for fastest graduation
+@fastestGrad_views.routes('/fastestGrad', methods=['GET'])
+def get_fastest_grad_route();
+    fastestRoute = fastestGraduation(Student)
+    return fastestRoute 
